c1a3f5ad0f2066be1b1923ab471939297a5cf110
martin kinyua <kinyuam664@gmail.com>
2026-03-01 16:44:21 +0300

    fix urls duplication

--- simply_city/mashimo/urls.py
+++ simply_city/mashimo/urls.py
@@ -2,12 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # your mashimo routes will go here
-]
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
+    path('', views.zones_map, name='home'),
     path('zones/', views.zones_map, name='zones_map'),
 ]
